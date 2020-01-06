890b8c58b14b80d5969bc0d2a6adff3d8495d5bc
Nicholas Fearns <you@example.com>
2020-01-06 21:22:52 +0000

    added Header and Footer

---
+++ src/components/ui/Footer.svelte
@@ -0,0 +1,26 @@
+<script>
+
+</script>
+
+<style>
+    p {
+        width: 100%;
+        max-width: 500px;
+        margin: 0 auto;
+        padding-left: 10px;
+        padding-right: 10px;
+        background-color: purple;
+        padding: 10px 0 ;
+        color: antiquewhite;
+        font-size: 20px;
+        font-weight: bold;
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen-Sans,
+         Ubuntu, Cantarell, 'Helvetica Neue', sans-serif;
+         text-align: center;
+    }
+</style>
+
+<div>
+    <p>FOOTER</p>
+    <p>more to come</p>
+</div>
