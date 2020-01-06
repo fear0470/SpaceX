568e1de927741c16c66040ae5c9595a0093c61c7
Nicholas Fearns <you@example.com>
2020-01-06 01:27:04 +0000

    trying to set up GraphQL with svelte

---
+++ src/components/App.svelte
@@ -0,0 +1,22 @@
+<script>
+  	import ApolloClient from "apollo-boost";
+	import { setClient } from "svelte-apollo";
+
+
+    const client = new ApolloClient({
+    uri: "http://api.spacex/graphql",
+
+    onError: ({ networkError, graphQLErrors }) => {
+      console.log("graphQLErrors", graphQLErrors);
+      console.log("networkError", networkError);
+    }
+  });
+  setClient(client);
+</script>
+
+
+<style>
+
+</style>
+
+<h1>hello</h1>
---
+++ src/components/SpaceX.svelte
@@ -0,0 +1,9 @@
+<script>
+  
+</script>
+
+<style>
+
+</style>
+
+<h1>SpaceX Information</h1>
